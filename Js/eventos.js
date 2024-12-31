d0877296bd30f69d13c5ad6278b6b20ee6f9155b
carlosnphp <carlosnascimentophp@gmail.com>
2024-12-31 19:54:57 +0000

    first commit

---
+++ Js/eventos.js
@@ -0,0 +1,15 @@
+const btnMenu = document.querySelector('.fa-bars')
+let navbar = document.querySelector('.navbar')
+
+btnMenu.addEventListener("click", function () {
+  if (navbar.classList.contains('navbar')) {
+    navbar.classList.remove('navbar')
+    btnMenu.setAttribute('class', 'fa-solid fa-xmark')
+
+  }
+  else {
+    navbar.classList.add('navbar')
+    btnMenu.setAttribute('class', 'fa-solid fa-bars')
+  }
+})
+
